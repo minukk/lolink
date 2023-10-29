d9ba05292f7de7f54d035c05e429f41c8063e682
minukk <alsdnr4320@gmail.com>
2023-10-30 07:44:34 +0900

    deploy

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -5,9 +5,7 @@ cd $REPOSITORY
 
 sudo /usr/bin/yarn
 #!/bin/bash
-REPOSITORY=/home/ubuntu/build
+cd /home/ubuntu/build
 
-cd $REPOSITORY
-
-/usr/bin/yarn
-/usr/bin/yarn start:dev
+yarn
+yarn start:dev
