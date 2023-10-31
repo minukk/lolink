223791940acb541e59303efe90053d71ad0b0e48
minukk <alsdnr4320@gmail.com>
2023-11-01 07:24:19 +0900

    deploy

--- lolink_back/src/main.ts
+++ lolink_back/src/main.ts
@@ -8,7 +8,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  //
   app.enableCors({
     origin: [
       'http://localhost:3000',
